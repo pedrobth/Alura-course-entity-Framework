c7b8ec9c191fa4cfe4f7bf038d73515e63139ea8
Pedro Risso <pedrobth@yahoo.com.br>
2021-12-03 08:51:34 -0300

    fix: update cinema dto and model | class

--- FilmesApi/Models/Cinema.cs
+++ FilmesApi/Models/Cinema.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using FilmesApi.Models;
 
 namespace FilmesAPI.Models
 {
@@ -12,7 +13,8 @@ namespace FilmesAPI.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "O campo de nome é obrigatório")]
         public string Nome { get; set; }
-        public int EnderecoFK { get; set; }
-        public int GerenteFK { get; set; }
+        public Endereco Endereco { get; set; }
+        public int EnderecoId { get; set; }
+        //public int GerenteFK { get; set; }
     }
 }
