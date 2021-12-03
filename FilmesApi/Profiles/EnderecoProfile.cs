8098ddd0c008201cfb73548323f3a02e2889f3da
Pedro Risso <pedrobth@yahoo.com.br>
2021-12-03 08:44:13 -0300

    fix: endereco map | profile completed

--- FilmesApi/Profiles/EnderecoProfile.cs
+++ FilmesApi/Profiles/EnderecoProfile.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using AutoMapper;
+using FilmesApi.Data.Dtos.Endereco;
+using FilmesApi.Models;
+
 namespace FilmesApi.Profiles
 {
-    public class EnderecoProfile
+    public class EnderecoProfile : Profile
     {
         public EnderecoProfile()
         {
+            CreateMap<CreateEnderecoDto, Endereco>();
+            CreateMap<Endereco, ReadEnderecoDto>();
+            CreateMap<UpdateEnderecoDto, Endereco>();
         }
     }
 }
