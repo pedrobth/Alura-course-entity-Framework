6ca01b981e87280b13112d476c6dc1ce7e0820d0
Pedro Risso <pedrobth@yahoo.com.br>
2021-12-03 08:50:10 -0300

    feat: add endereco controller and update csproj

---
+++ FilmesApi/Controllers/EnderecoController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos.Endereco;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public EnderecoController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<Endereco> RecuperaEnderecos()
+        {
+            return _context.Enderecos;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaEnderecoPorId(int id)
+        {
+            Endereco endereco = _context.Enderecos.FirstOrDefault(
+                endereco => endereco.Id == id);
+
+            if (endereco == null)
+            {
+                return NotFound();
+            }
+
+            ReadEnderecoDto enderecoDto = _mapper.Map<ReadEnderecoDto>(endereco);
+            return Ok(enderecoDto);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaEndereco([FromBody] CreateEnderecoDto enderecoDto)
+        {
+            Endereco endereco = _mapper.Map<Endereco>(enderecoDto);
+            _context.Enderecos.Add(endereco);
+            _context.SaveChanges();
+            return CreatedAtAction(
+                nameof(RecuperaEnderecoPorId), new { Id = endereco.Id }, endereco);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaEndereco(int id, [FromBody] UpdateEnderecoDto enderecoDto)
+        {
+            Endereco endereco = _context.Enderecos.FirstOrDefault(endereco => endereco.Id == id);
+            if (endereco == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(enderecoDto, endereco);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaEndereco(int id)
+        {
+            Endereco endereco = _context.Enderecos.FirstOrDefault(endereco => endereco.Id == id);
+            if (endereco == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(endereco);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
