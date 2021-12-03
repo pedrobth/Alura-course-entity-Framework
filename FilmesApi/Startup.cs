a6acd0af5636dd766ff1d3281adcc40dd7b80afc
Pedro Risso <pedrobth@yahoo.com.br>
2021-12-03 10:02:24 -0300

    fix: remove swagger from startup

--- FilmesApi/Startup.cs
+++ FilmesApi/Startup.cs
@@ -20,12 +20,14 @@ namespace FilmesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opts => opts.UseLazyLoadingProxies().UseMySQL(
+            services.AddDbContext<AppDbContext>(
+                opts => opts.UseLazyLoadingProxies().UseMySQL(
                 Configuration.GetConnectionString("CinemaConnection")));
 
             services.AddControllers();
 
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddAutoMapper(
+                AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,8 +36,6 @@ namespace FilmesAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmesAPI v1"));
             }
 
             app.UseHttpsRedirection();
