918bc6256508f4d49bf16a2b0346128763ad62be
Pedro Risso <pedrobth@yahoo.com.br>
2021-12-05 12:45:01 -0300

    fix: no redundant data @ get gerente/{id} route - change ReadGerenteDto - change GerenteProfile

--- FilmesApi/Profiles/GerenteProfile.cs
+++ FilmesApi/Profiles/GerenteProfile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using AutoMapper;
 using FilmesApi.Data.Dtos.Gerente;
 using FilmesApi.Models;
@@ -10,7 +11,16 @@ namespace FilmesApi.Profiles
         public GerenteProfile()
         {
             CreateMap<CreateGerenteDto, Gerente>();
-            CreateMap<Gerente, ReadGerenteDto>();
+            CreateMap<Gerente, ReadGerenteDto>()
+                .ForMember(gerente => gerente.Cinemas, opts => opts
+                    .MapFrom(gerente => gerente.Cinemas.Select(
+                        cine => new {
+                            cine.Id,
+                            cine.Nome,
+                            cine.Endereco
+                        }
+                    ))
+                );
         }
     }
 }
