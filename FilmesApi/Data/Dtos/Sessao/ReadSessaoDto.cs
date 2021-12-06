de8de3ac812521f3bb10ffbbeb69b3f2af9ea6c1
Pedro Risso <pedrobth@yahoo.com.br>
2021-12-06 17:09:17 -0300

    feat: + horario de inicio to sessao (runtime calc)

--- FilmesApi/Data/Dtos/Sessao/ReadSessaoDto.cs
+++ FilmesApi/Data/Dtos/Sessao/ReadSessaoDto.cs
@@ -12,5 +12,6 @@ namespace FilmesApi.Data.Dtos.Sessao
         public virtual Cinema Cinema { get; set; }
         public virtual Filme Filme { get; set; }
         public DateTime HorarioDeEncerramento { get; set; }
+        public DateTime HorarioDeInicio { get; set; }
     }
 }
---
+++ FilmesApi/Profiles/SessaoProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoMapper;
+using FilmesApi.Models;
+using FilmesApi.Data.Dtos.Sessao;
+
+namespace FilmesApi.Profiles
+{
+    public class SessaoProfile : Profile
+    {
+
+        public SessaoProfile()
+        {
+            CreateMap<CreateSessaoDto, Sessao>();
+            CreateMap<Sessao, ReadSessaoDto>()
+                .ForMember(dto => dto.HorarioDeInicio, opts => opts
+                .MapFrom(dto =>
+                    dto.HorarioDeEncerramento.AddMinutes(dto.Filme.Duracao *(-1))
+                    )
+                )
+            ;
+        }
+    }
+}
