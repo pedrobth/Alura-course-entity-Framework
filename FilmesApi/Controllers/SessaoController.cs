0fdcf75770c4cfafb9b1dcef0fbd04b32297f6df
Pedro Risso <pedrobth@yahoo.com.br>
2021-12-06 11:20:19 -0300

    feat: add sessao controller & dto

---
+++ FilmesApi/Controllers/SessaoController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos.Sessao;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SessaoController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public SessaoController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<Sessao> RecuperaSessoes()
+        {
+            return _context.Sessoes;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaSessaoPorId(int id)
+        {
+            Sessao sessao = _context.Sessoes.FirstOrDefault(
+                sessao => sessao.Id == id);
+
+            if (sessao == null)
+            {
+                return NotFound();
+            }
+
+            ReadSessaoDto sessaoDto = _mapper.Map<ReadSessaoDto>(sessao);
+            return Ok(sessaoDto);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaSessao(CreateSessaoDto dto)
+        {
+            Sessao sessao = _mapper.Map<Sessao>(dto);
+            _context.Sessoes.Add(sessao);
+            _context.SaveChanges();
+            return CreatedAtAction(
+                nameof(RecuperaSessaoPorId),
+                new { Id = sessao.Id },
+                sessao
+            );
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FilmesApi/Data/Dtos/Sessao/ReadSessaoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using FilmesAPI.Models;
+
+namespace FilmesApi.Data.Dtos.Sessao
+{
+    public class ReadSessaoDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public virtual Cinema Cinema { get; set; }
+        public virtual Filme Filme { get; set; }
+        public DateTime HorarioDeEncerramento { get; set; }
+    }
+}
